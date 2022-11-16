5c9f791b1dc68870c39abf343242c9b99429da45
Sergey <serjt27@gmai.com>
2022-11-16 23:13:18 +1000

    end check code bem readme

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,36 @@
 // делаем выборку дом элементов
-const popupElement = document.querySelector(".popup");
-const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-const profileElement = document.querySelector(".profile");
+let popupElement = document.querySelector(".popup");
+let popupCloseButtonElement = popupElement.querySelector(".popup__close");
+let popupOpenButtonElement = document.querySelector(".profile__edit-button");
+let profileElement = document.querySelector(".profile");
 
 // сохранение данных формы
 
-const mainForm = popupElement.querySelector(".popup__save");
+let mainForm = popupElement.querySelector(".popup__save");
 
 mainForm.addEventListener("click", saveFormClick);
 
 function saveFormClick() {
-  const FormName = popupElement.querySelector(".popup__name").value;
+  let FormName = popupElement.querySelector(".popup__name").value;
   document.querySelector(".profile__title").textContent = FormName;
 
-  const FormAbout = popupElement.querySelector(".popup__about").value;
+  let FormAbout = popupElement.querySelector(".popup__about").value;
   document.querySelector(".profile__subtitle").textContent = FormAbout;
   closePopup();
 }
 
 //подключаем popup
 
-const openPopup = function (event) {
+let openPopup = function (event) {
   popupElement.classList.add("popup_open");
   console.log("Open popup clicked");
 };
 
-const closePopup = function () {
+let closePopup = function () {
   popupElement.classList.remove("popup_open");
 };
 
-const closePopupByClickOnOverlay = function (event) {
+let closePopupByClickOnOverlay = function (event) {
   console.log(event.target, event.currentTarget);
   if (event.target !== event.currentTarget) {
     return;
