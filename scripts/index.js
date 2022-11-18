6bfc78be51f947fc710879ffee200d4708d37c72
Sergey <serjt27@gmai.com>
2022-11-18 20:45:47 +1000

    sitt prettier

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,32 @@
 // делаем выборку дом элементов
-let popupElement = document.querySelector(".popup");
-let popupCloseButtonElement = popupElement.querySelector(".popup__close");
-let popupOpenButtonElement = document.querySelector(".profile__edit-button");
-let profileElement = document.querySelector(".profile");
+let popupElement = document.querySelector('.popup');
+let popupCloseButtonElement = popupElement.querySelector('.popup__close');
+let popupOpenButtonElement = document.querySelector('.profile__edit-button');
+let profileElement = document.querySelector('.profile');
 
 // сохранение данных формы
 
-let mainForm = popupElement.querySelector(".popup__save");
+let mainForm = popupElement.querySelector('.popup__save');
 
-mainForm.addEventListener("click", saveFormClick);
+function saveFormClick(event) {
+  evt.preventDefault(event);
+  let FormName = popupElement.querySelector('.popup__name').value;
+  document.querySelector('.profile__title').textContent = FormName;
 
-function saveFormClick() {
-  let FormName = popupElement.querySelector(".popup__name").value;
-  document.querySelector(".profile__title").textContent = FormName;
-
-  let FormAbout = popupElement.querySelector(".popup__about").value;
-  document.querySelector(".profile__subtitle").textContent = FormAbout;
+  let FormAbout = popupElement.querySelector('.popup__about').value;
+  document.querySelector('.profile__subtitle').textContent = FormAbout;
   closePopup();
 }
 
 //подключаем popup
 
 let openPopup = function (event) {
-  popupElement.classList.add("popup_open");
-  console.log("Open popup clicked");
+  popupElement.classList.add('popup_open');
+  console.log('Open popup clicked');
 };
 
 let closePopup = function () {
-  popupElement.classList.remove("popup_open");
+  popupElement.classList.remove('popup_open');
 };
 
 let closePopupByClickOnOverlay = function (event) {
@@ -40,6 +39,7 @@ let closePopupByClickOnOverlay = function (event) {
 };
 
 // регистрируем обработчик событий по клику
-popupOpenButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
-popupElement.addEventListener("click", closePopupByClickOnOverlay);
+mainForm.addEventListener('submit', saveFormClick);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
