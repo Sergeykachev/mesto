e479b5cdc535387587cf97800130bb7686fb747e
Sergey <serjt27@gmai.com>
2022-12-12 22:47:24 +1000

    перенос массива 2 не сохр

--- scripts/index.js
+++ scripts/index.js
@@ -71,33 +71,6 @@ profileRemoveButton.addEventListener('click', profileCloseButton);
 
 //массив элементов карточек
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
-
 // Находим поля формы
 const profileForm = popupProfile.querySelector('.popup-profile__forms');
 const formCreateButton = profileForm.querySelector('.popup-profile__create');
